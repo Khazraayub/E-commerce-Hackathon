fd895ad9f214bc7c00a9c2129a19023a89d7f6e5
Khazra Ayub <khazra.ayub786@gmail.com>
2025-01-21 15:17:36 -0800

    update

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,26 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+// import { NextStudio } from 'next-sanity/studio'
+// import config from '../../../../sanity.config'
 
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
-export { metadata, viewport } from 'next-sanity/studio'
+// export { metadata, viewport } from 'next-sanity/studio'
 
-export default function StudioPage() {
-  return <NextStudio config={config} />
+// export default function StudioPage() {
+//   return <NextStudio config={config} />
+// }
+
+import React from 'react';
+import { NextStudio } from 'next-sanity/studio';
+import config from '../../../../sanity.config';
+
+export const dynamic = 'force-static';
+
+export { metadata, viewport } from 'next-sanity/studio';
+
+// Properly handling ReactNode return type
+export default function StudioPage(): React.ReactElement | null {
+  return <>{NextStudio({ config })}</>;
 }
