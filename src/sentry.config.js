6ca3469890b1423333b4b7a3087e36fb8c6b60e4
Khazra Ayub <khazra.ayub786@gmail.com>
2025-01-22 05:16:54 -0800

    sentry.config

---
+++ src/sentry.config.js
@@ -0,0 +1,12 @@
+const { withSentryConfig } = required("@sentry/nextjs");
+
+const moduleExports = {
+    // Your existing next.js config here
+};
+
+const SentryWebpackPluginOptions ={
+    silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+
